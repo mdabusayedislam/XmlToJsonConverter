7d4b5a438efef11ae709b6b60806498bdcbbc854
mdabusayedislam <abusayediit@gmail.com>
2022-12-10 00:47:38 +0600

    Add project files.

---
+++ XmlToJsonConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+
+const string DirectoryPath = @"E:\DotNet7\XmlToJsonConverter\XmlToJsonConverter\Data";
+
+string[] files = Directory.GetFiles(DirectoryPath);
+
+foreach (string fileName in files)
+{
+    ProcessFile(fileName);
+}
+
+ static void ProcessFile(string fileName)
+{
+    if (fileName.Contains(".xml"))
+    {
+        Console.WriteLine($"File Name {fileName}");
+        string xml = File.ReadAllText(fileName);
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xml);
+
+        Console.WriteLine("xml load completed");
+
+        string json = JsonConvert.SerializeXmlNode(doc);
+
+        Console.WriteLine("json convert completed");
+
+        File.WriteAllText(fileName.Replace(".xml", ".json"), json);
+
+        Console.WriteLine("json file save completed");
+    }    
+}
